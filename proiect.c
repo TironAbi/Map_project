852719cf5208ee775f901c21e7f7fc44321cf2b1
TironAbi <abigailtiron@gmail.com>
2025-01-06 17:18:18 +0200

    Update proiect.c

--- proiect.c
+++ proiect.c
@@ -1,6 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	int height;
+	//height il facem balance
+};
+
+int maximum(int a, int b);
+
+int height(struct Node *N)
+{
+	if (N == NULL)
+		return 0;
+	return N->height;
+}
+
+int maximum(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+struct Node* newNode(int key)
+{
+	struct Node* node = (struct Node*)
+		malloc(sizeof(struct Node));
+	node->key = key;
+	node->left = NULL;
+	node->right = NULL;
+	node->height = 1;  // new node is initially added at leaf 
+	return(node);
+}
+
  int main(){
-    printf("Hello eu");
+    
     
     return 0;
      }
