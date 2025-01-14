1bf8ea4ac0b8f38b8a4668797f55fa63247d7e03
TironAbi <abigailtiron@gmail.com>
2025-01-14 18:20:19 +0200

    functie inserare
    cream functia de inserare de elemente in arbore

--- proiect.c
+++ proiect.c
@@ -8,6 +8,7 @@ struct Node{
 	struct Node* right;
 };
 
+
 struct Node* createNode(int data){
 struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
 
@@ -16,14 +17,36 @@ newNode->left=NULL;
 newNode->right=NULL;
 
 return newNode;
+}
 
+
+struct Node* insert(struct Node* node, int data){
+	if(node==NULL){
+		return createNode(data);
+	}
+
+	if(data<node->data){
+		node->left=insert(node->left, data);
+	}
+	else{
+		node->right=insert(node->right, data);
+	}
+
+	return node;
 }
 
 
 
 
 int main(){
-	
+	struct Node* root=NULL;
+
+	int elemets[]={50, 30, 20, 40, 70, 60, 80};
+	int n=sizeof(elements)/sizeof(elements[0]);
+
+	for(int i=0; i<n; i++){
+		root=insert(root, elements[i]);
+	}
 
 
 	return 0;
